52e350fb8bd4c61008c55684c5c671b14e5538ae
volatile <irradiating@yandex.ru>
2018-10-31 14:33:35 +0300

    Initial commit

---
+++ dataset_helper.py
@@ -0,0 +1,263 @@
+# coding: utf8
+
+from keras.utils import np_utils
+import numpy as np
+import pandas as pd
+import os, sys, time, datetime, cv2
+
+
+def numpy_save(samples, labels, name):
+    np.savez_compressed(name, samples, labels)
+
+def numpy_restore(filename):
+    if os.path.exists(filename):
+        data = np.load(filename)
+        samples = data["arr_0"]
+        labels = data["arr_1"]
+        return samples, labels
+
+def labelfile_append(name="pupkin", filename="/opt/Project/dataset/labelfile.csv"):
+    if not os.path.exists(filename):
+        raise Exception("%s does not exist" %(filename))
+
+    strtime = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d%H%M%S")
+
+    set = pd.read_csv(filename, sep=":")
+
+    set = set.append([{"name":name,"time":strtime}], ignore_index=True)
+
+    set.to_csv(filename, sep=":", index=False)
+
+    max_index = set.last_valid_index()
+
+    print name, max_index, np_utils.to_categorical(max_index, 5000)
+
+def labelfile_get_by_name(name="pupkin", filename="/opt/Project/dataset/labelfile.csv"):
+    if not os.path.exists(filename):
+        raise Exception("%s does not exist" %(filename))
+
+    set = pd.read_csv(filename, sep=":")
+    max_index = set.last_valid_index()
+
+    for i in range(max_index+1):
+        if set.loc[i].values[0] == name:
+            return i, np_utils.to_categorical(i, 5000)
+
+    #print index, np_utils.to_categorical(index, 5000)
+
+def labelfile_get_by_index(index=2, filename="/opt/Project/dataset/labelfile.csv"):
+    if not os.path.exists(filename):
+        raise Exception("%s does not exist" %(filename))
+
+    set = pd.read_csv(filename, sep=":")
+    return set.loc[index].values[0]
+
+def initial_make_small_dir_chunks(root_dset_path):
+    trainbig_path = os.path.join(root_dset_path, "trainbig")
+    train_path = os.path.join(root_dset_path, "train")
+    test_path = os.path.join(root_dset_path, "test")
+    validate_path = os.path.join(root_dset_path, "validate")
+
+    for client_shortname in os.listdir(trainbig_path):
+        source_path_fullname = os.path.join(trainbig_path, client_shortname)
+        dst_path_fullname = os.path.join(validate_path, client_shortname)
+        counter = 0
+        for image_short_name in os.listdir(source_path_fullname):
+            if counter == 3000:
+                break
+            image_source_full_name = os.path.join(source_path_fullname, image_short_name)
+
+            try:
+                cv2.imread(image_source_full_name)
+            except:
+                os.remove(image_source_full_name)
+                continue
+
+            newshortname = "%s_%s.png" %(client_shortname, counter)
+            image_dst_full_name = os.path.join(dst_path_fullname, newshortname)
+
+            os.rename(image_source_full_name, image_dst_full_name)
+
+            counter += 1
+
+def dataset_assemble(path="/opt/Project/dataset/employee/"):
+
+    for setname in ["test", "validate"]:
+        set_path = os.path.join(path, setname)
+        set_save = os.path.join(path, "%s.npz" %(setname))
+
+        samples = []
+        labels = []
+        for label in os.listdir(set_path):
+            labelencode, labelonehot = labelfile_get_by_name(label)
+            images_label_dir = os.path.join(set_path, label)
+            for img_short_name in os.listdir(images_label_dir):
+                img_full_name = os.path.join(images_label_dir, img_short_name)
+                img = cv2.imread(img_full_name)
+
+                samples.append(img)
+                labels.append(labelencode)
+
+        samples = np.array(samples, dtype=np.uint8)
+        samples = samples.reshape(samples.shape[0], 512, 512, 3)
+        samples = samples.astype("float32")
+        samples /= 255
+
+        labels_onehot = np_utils.to_categorical(labels)
+
+        numpy_save(samples, labels_onehot, set_save)
+
+def dataset_smalllist_prepare(samplelist):
+    samples = []
+    labels = []
+
+    for label, samplename in samplelist:
+        if os.path.exists(samplename):
+            #print label, samplename
+            sample = cv2.imread(samplename)
+            sample = cv2.resize(sample, (139,139))
+            samples.append(sample)
+            labels.append(label)
+
+    samples = np.array(samples, dtype=np.float32)
+    samples /= 255
+    labels = np_utils.to_categorical(labels)
+    print samples.shape, labels.shape
+
+    return samples, labels
+
+def dataset_list(path="/opt/Project/dataset/faces", items=10):
+    items = 10
+
+    workdict = {}
+    finallist = []
+
+    for item in os.walk(path):
+        dir, _, namelist = item
+        #print dir, namelist
+
+        if not namelist:
+            continue
+
+        workdict[dir] = namelist
+
+    for item in range(items):
+
+        train = []
+        test = []
+        validate = []
+
+        for dir in workdict.keys():
+            try:
+                key = dir.split("/")[5]
+                codekey = labelfile_get_by_name(key)[0]
+                #print codekey
+                namelist = workdict[dir]
+
+            except:
+                continue
+
+            # if key == "junk" or key == "other" or key == "blur":
+            #     test_counter = 100
+            #     train_counter = 500
+            # else:
+            #     test_counter = 50
+            #     train_counter = 200
+
+            # print dir, len(workdict[dir]), len(workdict[dir])/10
+            chunk = len(workdict[dir]) / 10
+            test_counter = (chunk * 20) / 100
+            train_counter = (chunk * 60) / 100
+            #print key, chunk, test_counter, train_counter
+
+            for counter in range(test_counter):
+                try:
+                    name = os.path.join( dir, namelist.pop() )
+                    #if os.path.exists(name):
+                        #print name, codekey
+                    test.append( (codekey, name) )
+
+                    name = os.path.join( dir, namelist.pop() )
+                    #if os.path.exists(name):
+                        #print name, codekey
+                    validate.append( (codekey, name) )
+
+                    #print len(test), len(validate)
+                except:
+                    break
+
+            for counter in range(train_counter):
+                try:
+                    name = os.path.join(dir, namelist.pop())
+                    # if os.path.exists(name):
+                    #     print name, codekey
+                    train.append( (codekey, name) )
+
+                    #print len(train)
+                except:
+                    break
+
+        print item, len(train), len(test), len(validate)
+        if train and test and validate:
+            finallist.append([train, test, validate])
+
+    finallist.reverse()
+    return finallist
+
+def dataset_load(path="/opt/Project/dataset/faces/"):
+    train_set = os.path.join(path, "train.npz")
+    samples, labels = numpy_restore(train_set)
+    # print samples.shape
+    # print labels.shape, "\n"
+
+    test_set = os.path.join(path, "test.npz")
+    samples_test, labels_test = numpy_restore(test_set)
+    # print samples_test.shape
+    # print labels_test.shape, "\n"
+
+    validate_set = os.path.join(path, "validate.npz")
+    samples_validate, labels_validate = numpy_restore(validate_set)
+    # print samples_validate.shape
+    # print labels_validate.shape, "\n"
+
+    return samples, labels, samples_test, labels_test, samples_validate, labels_validate
+
+def resize_and_rename(source="/opt/Project/dataset/faces", dest="/mnt/nfs/webhost/Projects/dataset/faces"):
+    for subdir in os.listdir(source):
+
+
+        sourcedir = os.path.join(source, subdir)
+        destdir = os.path.join(dest, subdir)
+
+        if not os.path.isdir(destdir):
+            os.mkdir(destdir)
+
+        for short_name in os.listdir(sourcedir):
+            full_name = os.path.join(sourcedir, short_name)
+
+            timeform = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")
+            newname = "%s/%s.jpg" %( destdir, timeform )
+
+            img = cv2.imread(full_name)
+            img = cv2.resize(img, (128,128))
+            cv2.imwrite(newname, img)
+
+
+if __name__ == "__main__":
+    faces_path = "/opt/Project/dataset/faces/"
+    faces_128_path = "/opt/Project/dataset/faces_128/"
+
+    #initial_make_small_dir_chunks(faces_path)
+    #dataset_assemble(faces_path)
+    #dataset_load()
+
+    list = dataset_list(faces_128_path)
+    print len(list)
+    for train, test, validate in list:
+        print len(train), len(test), len(validate)
+
+    #resize_and_rename(faces_path, faces_128_path)
+
+    #labelfile_append("topkin")
+    #labelfile_get_by_name("topkin")
+    #labelfile_get_by_index(7)
---
+++ inception_model.py
@@ -0,0 +1,141 @@
+# coding: utf8
+
+from keras.applications import InceptionResNetV2
+from keras.applications import InceptionV3
+from keras.models import Model
+from keras.layers import Dense, Flatten
+from keras.utils import plot_model
+import os, cv2
+import numpy as np
+from dataset_helper import dataset_list, dataset_smalllist_prepare, labelfile_get_by_index
+
+
+def inception_resnet_v2_model_detector():
+    detector = InceptionResNetV2(include_top=False, weights="imagenet", input_shape=(139,139,3), pooling="none")
+    #print detector.summary()
+    #plot_model(model, to_file="inceptionv2.png", show_shapes=True)
+    return detector
+
+def inception_v3_model_detector():
+    #print help(InceptionV3)
+    detector = InceptionV3(include_top=False, weights="imagenet", input_shape=(139,139,3), pooling=None)
+    #print model.summary()
+    #plot_model(model, to_file="inception3.png", show_shapes=True)
+    return detector
+
+
+def inception_train_faces():
+    batch_size = 20
+    epochs = 200
+
+    if os.path.exists(weights_filename):
+        print "loading existing weights"
+        model.load_weights(weights_filename)
+
+    print "loading dataset"
+    datalist = dataset_list(faces_path)
+    for train, test, validate in datalist:
+        samples, labels = dataset_smalllist_prepare(train)
+        samples_test, labels_test = dataset_smalllist_prepare(test)
+        samples_validate, labels_validate = dataset_smalllist_prepare(validate)
+        print
+
+        continue
+
+        print "training model"
+        model.fit(samples, labels, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(samples_validate, labels_validate))
+        print "saving weights"
+        model.save_weights(weights_filename, overwrite=True)
+
+        try:
+            print "evaluating model"
+            loss, accuracy = model.evaluate(samples_test, labels_test, verbose=1)
+            print "model loss: %s, accuracy: %s" %(loss, accuracy)
+        except:
+            continue
+
+def use_model(model, samples):
+
+    samples = np.array(samples, dtype=np.float32)
+
+    samples = samples.reshape(samples.shape[0], image_shape[0], image_shape[1], image_shape[2])
+    samples /= 255
+
+    prediction = model.predict(samples)
+
+    index = np.argmax(prediction, axis=1)
+    confidence = prediction[0][index]
+
+    #return np.argmax(prediction, axis=1)
+    return confidence[0], index[0]
+
+def cycle_recognize_move(model, path="/opt/Project/dataset/camface"):
+    if os.path.exists(weights_filename):
+        print "loading saved weights"
+        model.load_weights(weights_filename)
+
+    for short_name in os.listdir(path):
+        full_name = os.path.join(path, short_name)
+
+        img = cv2.imread(full_name)
+        try:
+            img = cv2.resize(img, (139,139))
+        except:
+            continue
+
+        confidence, prediction = use_model(model, [img])
+        name = labelfile_get_by_index(prediction)
+
+        print name, confidence
+
+        if confidence >= 0.9:
+            name = "%s_09" %(name)
+        elif confidence >= 0.8 and confidence < 0.9:
+            name = "%s_08" %(name)
+        elif confidence >= 0.7 and confidence < 0.8:
+            name = "%s_07" %(name)
+        else:
+            name = "unconfident"
+
+        dstdir = os.path.join(path, name)
+
+        if not os.path.exists(dstdir):
+            os.mkdir(dstdir)
+
+        dst_filename = os.path.join(dstdir, short_name)
+        os.rename(full_name, dst_filename)
+
+
+
+def make_precompiled_model():
+
+    detector = inception_resnet_v2_model_detector()
+
+    # make detector layers not trainable
+    for layer in detector.layers:
+        # print layer
+        layer.trainable = False
+
+    x = Flatten()(detector.output)
+    x = Dense(4096, activation="relu")(x)
+    x = Dense(num_labels, activation="softmax")(x)
+
+    model = Model(detector.input, x)
+
+    model.compile(optimizer="rmsprop", loss="categorical_crossentropy")
+
+    print model.summary()
+
+    return model
+
+
+
+
+if __name__ == "__main__":
+    weights_filename = "inception_resnet_v2.hdf"
+    faces_path = "/opt/Project/dataset/faces"
+    num_labels = 188
+
+    model = make_precompiled_model()
+
+    inception_train_faces()
